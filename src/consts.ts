36ad6c384d76e7cf37c41456ec928f5c7db23f83
Steve Hall <srhusa@outlook.com>
2026-02-12 18:11:39 -0500

    site update

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Steve Hall · Builder of Calm Software';
+export const SITE_TITLE = 'Steve Hall';
 export const SITE_DESCRIPTION =
-	"Notes on software craftsmanship, product thinking, and the experiments I'm running.";
+	"Notes on software, product thinking, and the experiments I'm running.";
